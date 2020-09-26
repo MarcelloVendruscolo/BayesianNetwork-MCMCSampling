398c78007b87d22a722fd4016c5ef364c4ba4f2e
MarcelloVendruscolo <marcello.pietro@hotmail.com>
2020-09-26 12:25:37 +0200

    Initial code structure with main functions and arguments

--- learn_diagnose.R
+++ learn_diagnose.R
@@ -5,11 +5,19 @@ library()
 install.packages("/Users/marcellovendruscolo/Documents/rstudio-workspace/Diagnostics/Diagnostics_1.2.0.tar.gz", repos = NULL, type = "source")
 
 #Load Diagnostics package for this session
-library(DeliveryMan)
+library(Diagnostics)
 
 #List all the packages currently loaded
 search()
 
+
+#Function to create and learn the Bayesian network. The only parameter is the historical medical data.
 learn <- function(historical_data) {
-  
+  print(historical_data)
 }
+
+diagnose <- function(network, cases) {
+  print("diagnose function")
+}
+
+runDiagnostics(learn, diagnose, verbose = 0)
